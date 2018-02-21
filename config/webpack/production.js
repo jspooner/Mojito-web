152aaa2fbd8a088aefe72b7fd2ff9ab1dcbc5323
Jonathan Spooner <jspooner@gmail.com>
2018-02-21 06:45:34 -0800

    Prod build source maps

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,9 +1,15 @@
+const path = require('path');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./common');
 
 module.exports = merge(common, {
+  devtool: 'source-map',
   plugins: [
-    new UglifyJSPlugin()
-  ]
+    new UglifyJSPlugin({sourceMap: true})
+  ],
+  output: {
+    path: path.resolve(__dirname, '../../dist'),
+    filename: '[name].js'
+  },
 });
