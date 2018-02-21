f8e5ae63a8c680a7bb7ea5184b29bec63f6e8edb
Jonathan Spooner <jspooner@gmail.com>
2018-02-21 06:50:15 -0800

    Prod build NODE_ENV

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./common');
@@ -6,7 +7,8 @@ const common = require('./common');
 module.exports = merge(common, {
   devtool: 'source-map',
   plugins: [
-    new UglifyJSPlugin({sourceMap: true})
+    new UglifyJSPlugin({sourceMap: true}),
+    new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify('production')})
   ],
   output: {
     path: path.resolve(__dirname, '../../dist'),
